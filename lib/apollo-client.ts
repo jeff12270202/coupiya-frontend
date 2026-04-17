d65a9b6613bb0445487139341362580cd37df43d
jeff12270202 <jefflao12270202@outlook.com>
2026-04-17 21:32:27 +0800

    fix: 分离商品列表组件，禁用 SSR 解决白屏

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 const httpLink = createHttpLink({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || 'https://api.coupiya.com/saleor/graphql/',
+  uri: process.env.NEXT_PUBLIC_SALEOR_API_URL || 'https://api.coupiya.com/saleor/graphql/',
 });
 
 export const client = new ApolloClient({
